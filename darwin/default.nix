a51da3aaa27c89431c90517d67a4f1135833dc07
Glenn Gillen <github@gln.io>
2025-11-30 23:34:59 +1100

    Setting some of the mac defaults.

--- darwin/default.nix
+++ darwin/default.nix
@@ -62,4 +62,10 @@
     ];
     pathsToLink = [ "/Applications" ];
   };
+
+  system.activationScripts.activateSettings.text = ''
+    # Following line should allow us to avoid a logout/login cycle
+    /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+  '';
+
 }
