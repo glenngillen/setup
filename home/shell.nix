a91ecb067a28ce49e24dd304268e75bd01ae8396
Glenn Gillen <me@glenngillen.com>
2025-12-08 19:19:34 +1100

    Icon for development path

--- home/shell.nix
+++ home/shell.nix
@@ -52,7 +52,7 @@ _: {
           "Downloads" = " ";
           "Music" = " ";
           "Pictures" = " ";
-          "Development" = "󱃖 "
+          "Development" = "󱃖 ";
         };
       };
     };
