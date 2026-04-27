6ea41360cdaf0540372f207a51b785a3c72262ad
Glenn Gillen <me@glenngillen.com>
2026-04-27 22:47:59 +1000

    updating licecap window following script

--- home/configs/aerospace-workspace-change.sh
+++ home/configs/aerospace-workspace-change.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-window_id=$(aerospace list-windows --monitor all --app-bundle-id co.boomvision.boom.macos | grep -i remote | cut -f 1 -d" ")
 target_ws=$(aerospace list-workspaces --monitor mouse --visible)
 
+window_id=$(aerospace list-windows --monitor all --app-bundle-id co.boomvision.boom.macos | grep -i remote | cut -f 1 -d" ")
 aerospace move-node-to-workspace --window-id "$window_id" "$target_ws"
+
+window_id=$(aerospace list-windows --monitor all --app-bundle-id com.cockos.LICEcap | cut -f 1 -d" ")
 aerospace move-node-to-workspace --window-id "$window_id" "$target_ws"
